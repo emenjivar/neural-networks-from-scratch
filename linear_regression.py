bd7f35abe19b2921b5afa0f854f333459eeb8187
Carlos Menjivar <carlosmenjivar@ravn.co>
2023-01-06 22:18:20 -0600

    Print iteration number on linear regression

--- linear_regression.py
+++ linear_regression.py
@@ -10,16 +10,16 @@ def predict(i): #equation of line f(x)=mx where m->w, i->x
     return w*i
 
 # training the network
-for _ in range(25):
+for iteration in range(25):
     pred = [predict(i) for i in inputs]
     errors = [t-p for p,t in zip(pred, targets)]
     cost = sum(errors) / len(targets) # indicate how closest are the approximations
-    #print(f"weight: {w:.2f}, cost: {cost:.2f}")
+    # print(f"iteration: {iteration + 1}, weight: {w:.2f}, cost: {cost:.2f}")
     w += cost * learning_rate # updating the weight in the network is called back propagation
 
 # test the network
 test_inputs = [5, 6]
-test_targets = [10,12]
+test_targets = [10, 12]
 pred = [predict(i) for i in test_inputs]
 
 for i,t,p in zip(test_inputs, test_targets, pred):
